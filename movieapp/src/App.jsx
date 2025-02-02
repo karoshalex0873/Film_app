d879c784c6f197caa53ef053d97e40ad6190339b
Alex Karobia <akarobia30@gmail.com>
2025-02-02 14:08:03 +0300

    Understanding the styling technigues used in react

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,21 +1,26 @@
-const Card = ({title}) => {
+const Card = ({ title, ReleaseDate, Rating }) => {
   return (
     <>
-     <h2>{title}</h2>
+      <div className="card">
+        <h2>{title}</h2>
+        <h4>{ReleaseDate}</h4>
+        <h5>{Rating}</h5>
+      </div>
     </>
-  )
-}
-
+  );
+};
 
 const App = () => {
   return (
     <>
       <p>Props (properties) in react native </p>
       <h1>Movie list</h1>
-      < Card title={"Star wars"}/>
-      < Card title={"The lion King"}/>
-      < Card title={"Avata"}/>
+      <div className="Card_con">
+        <Card title={"Star wars"} ReleaseDate={"2022"} Rating={7.8} />
+        <Card title={"The lion King"} ReleaseDate={"2024"} />
+        <Card title={"Avatar"} ReleaseDate={"2018"} />
+      </div>
     </>
-  )
-}
-export default App
+  );
+};
+export default App;
